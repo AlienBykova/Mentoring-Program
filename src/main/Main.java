f18a16c2d4dfa3fa431385b26a3bf3f5ecb8f3d8
Alona.Bykova <bykova.alien@gmail.com>
2018-04-02 22:06:55 +0300

    Add Pi evaluation

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,29 @@
 package main;
 
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Double PI = Double.valueOf(String.format(Locale.ENGLISH, "%.10f", calculatePi(170)));
+        Double piFromMath = Double.valueOf(String.format(Locale.ENGLISH, "%.10f", Math.PI));
+        if (PI == piFromMath) {
+            System.out.println("Equals!");
+        } else {
+            System.out.println(PI);
+            System.out.println(piFromMath);
+            System.out.println("No");
+        }
+    }
+
+    public static double calculatePi(int iterationsCount) {
+        double pi = 3;
+
+        for (int i = 0, base = 2; i < iterationsCount; i++, base += 2) {
+            pi += (i % 2 == 0 ? 1 : -1) * 4.0 / (base * (base + 1) * (base + 2));
+        }
+
+        return pi;
     }
 }
+
